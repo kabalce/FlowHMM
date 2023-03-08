c2c4da006f3087ae5604d9b9b9fffceb93fc24bc
Klaudia <k.p.balcer@gmail.com>
2023-03-08 12:25:37 +0100

    debug

--- source/model/discretized_HMM.py
+++ source/model/discretized_HMM.py
@@ -26,10 +26,10 @@ class DiscreteHMM(hmm.CategoricalHMM):
         self.nodes = X[np.random.choice(X.shape[0], size=self.no_nodes, replace=False)]
 
     def _provide_nodes_latin(self, X):  # each point in a row
-        self.nodes = np.apply_along_axis(lambda x: np.quantile(x[:(-self.no_nodes)], x[(-self.no_nodes):]), 0, np.concatenate([X, LatinHypercube(X.shape[1]).random(self.no_nodes)],  axis=0)).transpose()
+        self.nodes = np.apply_along_axis(lambda x: np.quantile(x[:(-self.no_nodes)], x[(-self.no_nodes):]), 0, np.concatenate([X, LatinHypercube(self.no_nodes).random(X.shape[1]).transpose()],  axis=0)).transpose()
 
     def _provide_nodes_uniform(self, X):
-        np.random.uniform(size=10).reshape(-1, 1) @ (X.max(axis=0) - X.min(axis=0)).reshape(1, -1) + X.min(axis=0)
+        self.nodes = np.random.uniform(size=10).reshape(-1, 1) @ (X.max(axis=0) - X.min(axis=0)).reshape(1, -1) + X.min(axis=0)
 
     def _provide_nodes(self, X, force):
         if not force and (self.nodes is not None):
@@ -62,3 +62,6 @@ if __name__ == "__main__":  # TODO: provide a test for an simple example
     myHMM3 = DiscreteHMM('latin_cube', 10, 3)
     obs, hid = hmm.sample(100)
     myHMM.fit(obs)
+    myHMM2.fit(obs)
+    myHMM3.fit(obs)
+    # TODO: visualize the nodes
