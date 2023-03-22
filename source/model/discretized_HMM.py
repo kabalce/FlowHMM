6f97cd885ca81c07a56295d54d989d87589a6e1f
Klaudia <k.p.balcer@gmail.com>
2023-03-22 13:03:32 +0100

    update model to infer from gaussianHmm

--- source/model/discretized_HMM.py
+++ source/model/discretized_HMM.py
@@ -1,26 +1,35 @@
 from hmmlearn import hmm
 import numpy as np
 from scipy.stats.qmc import LatinHypercube
-
+# import tensorflow as tf
+import torch
 
 DISCRETIZATION_TECHNIQUES = ['random', 'latin_cube_u', 'latin_cube_q', 'uniform']
 
 
-class DiscreteHMM(hmm.CategoricalHMM):
-    def __init__(self, discretization_method: str = 'random', number_of_nodes: int = 100,
-                 n_components=1, startprob_prior=1.0, transmat_prior=1.0, *, emissionprob_prior=1.0,  # n_features=None,
-                 algorithm='viterbi', random_state=None, n_iter=10, tol=0.01, verbose=False, params='ste',
-                 init_params='ste', implementation='log') -> None:
-        super().__init__(n_components, startprob_prior, transmat_prior,
-                         emissionprob_prior=emissionprob_prior,  # n_features=n_features,
-                         algorithm=algorithm, random_state=random_state, n_iter=n_iter, tol=tol, verbose=verbose,
-                         params=params, init_params=init_params, implementation=implementation)
+class DiscreteHMM(hmm.GaussianHMM):
+    def __init__(self,
+                 discretization_method: str = 'random', number_of_nodes: int = 100,
+                 n_components=1, startprob_prior=1.0, transmat_prior=1.0,
+                 covariance_type='diag', min_covar=0.001,
+                 means_prior=0, means_weight=0, covars_prior=0.01, covars_weight=1,
+                 algorithm='viterbi', random_state=None, n_iter=10, tol=0.01, verbose=False, params='stmc',
+                 init_params='stmc', implementation='log') -> None:
+
+        super(DiscreteHMM, self).__init__(
+            n_components=n_components, covariance_type=covariance_type, min_covar=min_covar,
+            startprob_prior=startprob_prior, transmat_prior=transmat_prior,
+            means_prior=means_prior, means_weight=means_weight, covars_prior=covars_prior, covars_weight=covars_weight,
+            algorithm=algorithm, random_state=random_state, n_iter=n_iter, tol=tol, verbose=verbose,
+            params=params, init_params=init_params, implementation=implementation)
+
         assert discretization_method in DISCRETIZATION_TECHNIQUES, \
             f"discretization method: '{discretization_method}' not allowed, choose one of {DISCRETIZATION_TECHNIQUES}"
+
         self.discretization_method = discretization_method
         self.no_nodes = number_of_nodes
-        # Placeholders
-        self.nodes = None
+        self.nodes = None  # Placeholder
+        self.optimizer = None  # TODO
 
     def _provide_nodes_random(self, X):
         self.nodes = X[np.random.choice(X.shape[0], size=self.no_nodes, replace=False)].transpose()
@@ -51,13 +60,25 @@ class DiscreteHMM(hmm.CategoricalHMM):
 
     def _discretize(self, X, force):
         self._provide_nodes(X, force)
-        return np.argmin(np.square(X[:, :, np.newaxis] - self.nodes[np.newaxis, :, :]).sum(axis=1), axis=1).reshape(-1, 1)
+        return self.nodes.T[np.argmin(np.square(X[:, :, np.newaxis] - self.nodes[np.newaxis, :, :]).sum(axis=1), axis=1).reshape(-1)]  # zwraca ciągłe
 
-    def fit(self, X, lengths=None, force=False):
+    def _init(self, X, lengths=None):  # TODO
+        super()._init(X)
+        if self._needs_init("s", "startprob_"):
+            self._startprob_tensor = torch.tensor(self.startprob_, requires_grad=True)
+        if self._needs_init("t", "transmat_"):
+            self._transmat_tensor = torch.tensor(self.transmat_, requires_grad=True)
+        if self._needs_init("m", "means_"):
+            self._means_tensor =torch.tensor(self.means_, requires_grad=True)
+        if self._needs_init("c", "covars_"):
+            self._covars_tensor = torch.tensor(self.covars_, requires_grad=True)
+
+
+    def fit(self, X, lengths=None, force=False):  # TODO
         Xd = self._discretize(X, force)
-        super().fit(Xd, lengths)
+        super().fit(Xd, lengths)  # TODO: contains init, rewrite
 
-    def _do_mstep(self, stats):
+    def _do_mstep(self, stats):  # TODO
         super()._do_mstep(stats)
 
 
@@ -72,4 +93,3 @@ if __name__ == "__main__":  # TODO: provide a test for an simple example
     myHMM2.fit(obs)
     myHMM3.fit(obs)
     myHMM4.fit(obs)
-    # TODO: visualize the nodes
