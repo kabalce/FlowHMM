1c17c99e0db72c69300213cc701289857dd900d9
Klaudia <k.p.balcer@gmail.com>
2023-04-23 19:17:23 +0200

    debug

--- clickstream_experiment/source/train_hmm.sh
+++ clickstream_experiment/source/train_hmm.sh
@@ -8,7 +8,7 @@ do
     do
       for n in 10 50 100 1000 10000
       do
-        python3 train_hmm.py --w2v-dim $d --w2v-epochs $e --w2v-min-len $l --hmm-nodes $n -hmm-min-len $l
+        python3 train_hmm.py --w2v-dim $d --w2v-epochs $e --w2v-min-len $l --hmm-nodes $n --hmm-min-len $l
       done
     done
   done
