db4a18737d7c310561f79a55a907395584914dc6
Klaudia <k.p.balcer@gmail.com>
2023-06-16 16:48:02 +0200

    new preprocessing

---
+++ clickstream_experiment/source/preprocessing_less_products.py
@@ -0,0 +1,221 @@
+import argparse
+import pickle as pkl
+import pandas as pd
+import numpy as np
+from pathlib import Path
+import os
+import matplotlib.pyplot as plt
+from gensim.models import Word2Vec
+from gensim.models.word2vec import LineSentence
+import logging
+import sys
+from collections import defaultdict as dd
+
+PROJECT_PATH = f"{Path(__file__).absolute().parent.parent.parent}"
+sys.path.insert(1, PROJECT_PATH)
+from clickstream_experiment.source.clickstream import ClickStream, zero_init
+
+
+DATA_SET = "test"
+logging.basicConfig(
+    filename=f"{PROJECT_PATH}/clickstream_experiment/logs/preprocessing.log",
+    encoding="utf-8",
+    level=logging.DEBUG,
+)
+
+def parse_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "--w2v-dim", type=int, default=100, help="length of word2vec embedding"
+    )
+    parser.add_argument(
+        "--w2v-epochs",
+        type=int,
+        default=50,
+        help="number of epochs for word2vec training",
+    )
+    parser.add_argument(
+        "--w2v-min-len",
+        type=int,
+        default=10,
+        help="minimal sequence length for word3vec training",
+    )
+    args = parser.parse_args()
+    return args.w2v_dim, args.w2v_epochs, args.w2v_min_len
+
+
+def load_raw_clickstream():
+    with open(
+        f"{PROJECT_PATH}/clickstream_experiment/data/raw_data/{DATA_SET}.jsonl"
+    ) as f:
+        click_stream = ClickStream(f)
+    with open(
+        f"{PROJECT_PATH}/clickstream_experiment/data/preprocessed_data/ClickStream_{DATA_SET}.pkl",
+        "wb",
+    ) as f:
+        pkl.dump(click_stream, f)
+    return click_stream
+
+
+def analyze_clickstream(click_stream):
+    # TODO: analiza typów sesji
+
+    session_lens = pd.Series(click_stream.session_lengths)
+    item_ids = pd.Series(click_stream.item_ids)
+
+    print(f"Number of sessions in {DATA_SET} set: {len(click_stream.sessions)}")
+    logging.debug(f"Number of sessions in {DATA_SET} set: {len(click_stream.sessions)}")
+    print(f"Number of products in {DATA_SET} set: {item_ids.shape[0]}")
+    logging.debug(f"Number of products in {DATA_SET} set: {item_ids.shape[0]}")
+    print(
+        f"Average session length in {DATA_SET} set: {(session_lens.index * session_lens).sum() / session_lens.sum()}"
+    )  # TODO: fix the statistics
+    logging.debug(f"Average session length in {DATA_SET} set: {session_lens.mean()}")
+
+    session_lens.sort_index().plot(kind="bar")
+    plt.savefig(
+        f"{PROJECT_PATH}/clickstream_experiment/analysis/sesstion_lens_{DATA_SET}.png"
+    )
+
+    item_ids.value_counts().sort_index().plot(kind="hist")
+    plt.xlabel("Number of occurrences")
+    plt.ylabel("Number of products")
+    plt.savefig(
+        f"{PROJECT_PATH}/clickstream_experiment/analysis/item_freqs_{DATA_SET}.png"
+    )
+
+
+def prepare_file_for_w2v(click_stream, w2v_min_len):
+    with open(
+        f"{PROJECT_PATH}/clickstream_experiment/data/preprocessed_data/sequences_{w2v_min_len}.txt",
+        "w",
+    ) as f:
+        [
+            f.write(" ".join([str(w) for w in s.list_items()]) + "\n")
+            for s in click_stream.sessions
+            if len(s.event_list) >= w2v_min_len
+        ]
+
+
+def prepare_df_for_tagnn(click_stream, w2v_min_len):
+    with open(
+        f"{PROJECT_PATH}/clickstream_experiment/data/preprocessed_data/TAGNN_df_{w2v_min_len}_{DATA_SET}.dat",
+        "w",
+    ) as f:
+        [   # 'sessionID', 'timestamp', 'itemID', 'category'
+            f.write(f"{s.session_id} {e.ts} {e.item_id} {e.type}" + "\n")
+            for s in click_stream.sessions
+            for e in s.event_list
+            if len(s.event_list) >= w2v_min_len
+        ]
+
+
+def prepare_file_for_tagnn(click_stream, w2v_min_len):
+    l_ = len(click_stream.sessions)
+    indexes = np.random.choice(l_, size=l_ // 8, replace=False)
+
+    data = (
+        [
+            s.list_items()[:-1]
+            for i, s in enumerate(click_stream.sessions)
+            if (len(s.event_list) >= w2v_min_len) and (i in indexes)
+        ],
+        [
+            s.list_items()[-1]
+            for i, s in enumerate(click_stream.sessions)
+            if (len(s.event_list) >= w2v_min_len) and (i in indexes)
+        ]
+    )
+
+    tagnn_path = f"{PROJECT_PATH}/clickstream_experiment/data/preprocessed_data/TAGNN_seq_{w2v_min_len}_{DATA_SET}.pkl"
+
+    with open(
+            tagnn_path,
+            "wb",
+    ) as f:
+        pkl.dump(data, f)
+
+
+def train_w2v(w2v_dim, w2v_epochs, w2v_min_len, min_count):
+    w2v_model = Word2Vec(
+        sentences=LineSentence(
+            f"{PROJECT_PATH}/clickstream_experiment/data/preprocessed_data/sequences_{w2v_min_len}.txt"
+        ),
+        vector_size=w2v_dim,
+        window=5,
+        min_count=min_count,
+        workers=30,
+        epochs=w2v_epochs,
+    )
+    w2v_model.wv.save(
+        f"{PROJECT_PATH}/clickstream_experiment/data/preprocessed_data/vectors_train_{w2v_dim}_{w2v_min_len}_{w2v_epochs}_{min_count}.kv"
+    )
+
+
+def tidy_up(cs, cs_path_cleaned):
+    # znajdź rzadkie produkty
+    freqs = pd.Series(cs.item_ids).sort_values()
+    rare_products = set(freqs.loc[freqs < 5].index.values.tolist())
+
+    # usuń rzadkie produkty z sesji
+    [setattr(s, 'event_list', list(set(s) - rare_products)) for s in cs.sessions]
+
+    # usuń za krótkie sesje
+    cs.sessions = [s for s in cs.sessions if len(s) > 2]
+
+    # zaktualizuj atrybuty w cs
+    cs.item_ids = dd(zero_init)
+    cs.session_lengths = dd(zero_init)
+
+    for s in cs.sessions:
+        cs.session_lengths[len(s)] += 1
+        for e in s.event_list:
+            cs.item_ids[e.item_id] += 1
+
+    with open(
+            cs_path_cleaned,
+            "wb",
+    ) as f:
+        pkl.dump(cs, f)
+
+    return cs
+
+
+if __name__ == "__main__":
+    # logging.debug('Start preprocessing.')
+    w2v_dim, w2v_epochs, w2v_min_len = parse_args()
+
+    min_count = 8
+
+    cs_path = f"{PROJECT_PATH}/clickstream_experiment/data/preprocessed_data/ClickStream_{DATA_SET}.pkl"
+    cs_path_cleaned = f"{PROJECT_PATH}/clickstream_experiment/data/preprocessed_data/ClickStream_{DATA_SET}_cleaned.pkl"
+    w2v_path = f"{PROJECT_PATH}/clickstream_experiment/data/preprocessed_data/sequences_{w2v_min_len}_cleaned.txt"
+    tagnn_path = f"{PROJECT_PATH}/clickstream_experiment/data/preprocessed_data/TAGNN_seq_{w2v_min_len}_{DATA_SET}_cleaned.pkl"
+    tagnn_df_path = f"{PROJECT_PATH}/clickstream_experiment/data/preprocessed_data/TAGNN_df_{w2v_min_len}_{DATA_SET}_cleaned.dat"
+
+    for path, prepare_file_fun in zip([w2v_path, tagnn_path, tagnn_df_path],
+                                      [prepare_file_for_w2v, prepare_file_for_tagnn, prepare_df_for_tagnn]):
+        if not os.path.exists(path):
+            if os.path.exists(cs_path):
+                with open(
+                    cs_path,
+                    "rb",
+                ) as f:
+                    cs = pkl.load(f)
+            else:
+                cs = load_raw_clickstream()
+
+            cs = tidy_up(cs, cs_path_cleaned)
+
+            """
+            Number of sessions in train set: 12899779
+            Number of products in train set: 1855603
+            Average session length in train set: 16.799985178040647
+            """
+
+            logging.info("ClickStream loaded.")
+            prepare_file_fun(cs, w2v_min_len)
+            logging.info("Data for word2vec written to file.")
+            del cs
+
+    train_w2v(w2v_dim, w2v_epochs, w2v_min_len, min_count)
